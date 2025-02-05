d58396e57481a4d9ee14e991dbb6d52f62eba6b1
Guru Ramu <guru04041997@gmail.com>
2025-02-05 22:16:59 +0530

    fix: create connection

--- api/src/middleware/response.ts
+++ api/src/middleware/response.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from 'express';
 
 export const addRespondToResponse: RequestHandler = (_req, res, next) => {
-  (res as any).respond = (data): void => {
+  (res as any).respond = (data: any): void => {
     res.status(200).send(data);
   };
   next();
